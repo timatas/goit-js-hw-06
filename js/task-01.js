69616fc350995a3b41c2346e44f02ac36bc4bdc1
timatas <139263028+timatas@users.noreply.github.com>
2023-10-27 21:30:33 +0300

    task 1-3

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+"use strict";
+const numberCategory = document.querySelector("#categories");
+console.log("Number of categories: ", numberCategory.children.length);
+
+const item = document.querySelectorAll(".item");
+[...item].forEach((el) => {
+  console.log("Category: ", el.firstElementChild.textContent);
+  console.log("Elements: ", el.getElementsByTagName("li").length);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listIngredients = document.querySelector("#ingredients");
+const markup = ingredients.map((ingrelient) => {
+  const liElement = document.createElement("li");
+  liElement.classList.add("item");
+  liElement.textContent = ingrelient;
+  return liElement;
+});
+listIngredients.append(...markup);
