feca2f7957e19156fb854fce77c9e5cb9d66cc2d
timatas <139263028+timatas@users.noreply.github.com>
2023-11-04 11:17:17 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -5,17 +5,20 @@ const boxes = document.querySelector("#boxes");
 
 createBtn.addEventListener("click", onCreateBtn);
 function onCreateBtn(evt) {
+  evt.preventDefault();
   createBoxes(input.value);
 }
+
+let style = {
+  width: 30,
+  height: 30,
+  color: getRandomHexColor(),
+};
+let { width, height, color } = style;
+let arrayDiv = [];
+
 function createBoxes(amount) {
-  let style = {
-    width: 30,
-    height: 30,
-    color: getRandomHexColor(),
-  };
-  let { width, height, color } = style;
-  let arrayDiv = [];
-  for (let i = 1; i <= amount; i++) {
+  for (let i = 0; i < amount; i++) {
     arrayDiv[
       i
     ] = `<div class = "div-item" style = "width: ${width}px; height: ${height}px; background-color: ${color}"></div>`;
@@ -24,9 +27,9 @@ function createBoxes(amount) {
     color = getRandomHexColor();
   }
   const markup = arrayDiv.join("");
-  // boxes.innerHTML = markup;
   boxes.insertAdjacentHTML("beforeend", markup);
 }
+
 const destroyBtn = document.querySelector("button[data-destroy]");
 destroyBtn.addEventListener("click", destroyBoxes);
 function destroyBoxes() {
